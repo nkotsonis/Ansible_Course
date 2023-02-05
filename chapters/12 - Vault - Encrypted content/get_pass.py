a706344665ad019eeff76db54bd2b052a60e398e
Nikos Kotsonis <nkotsonis@gmail.com>
2023-02-05 21:49:53 +0000

    first commit

---
+++ chapters/12 - Vault - Encrypted content/get_pass.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+# This script could fetch the password from a password vault 
+
+print("dfDfDFs#34Ss@#gh42So")    # The password that is output in stdout, will be used for encryption
